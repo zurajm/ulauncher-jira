8ebb2d9a232be6f0e241bd6044c86fc4c3752abf
zurajm <miha@zuraj.com>
2021-06-03 09:07:35 +0200

    Fix for gi

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 """ Created by Safa Arıman on 10.12.2018 """
 """ Updated by David Hollinger on 08.21.2019 """
+import gi
+gi.require_version('Gdk', '3.0')
 from jira.extension import JiraExtension
 
 __author__ = 'dhollinger'
